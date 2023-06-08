9ec999827cc00455cbcb1355db4f032312ffc8d4
TeunGit <afm.lauwerijssen@student.avans.nl>
2023-06-08 20:48:44 +0200

    renamed suit to foundation
    this to more accuratly describe the class name

--- src/Foundation.java
+++ src/Foundation.java
@@ -1,8 +1,8 @@
 import java.awt.geom.Point2D;
 import java.util.Queue;
 
-public class Suit extends Stock{
-    public Suit(Point2D position, Queue<Card> stockCards) {
+public class Foundation extends Stock{
+    public Foundation(Point2D position, Queue<Card> stockCards) {
         super(position, stockCards);
     }
 
